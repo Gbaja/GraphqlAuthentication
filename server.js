b4fb84c2157b31a47c823eda1c6ce6ea1e520e03
Gbaja <gbajaf@yahoo.co.uk>
2018-04-09 14:36:11 +0100

    Renamed backend to server and added package lock to gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const server = require("./backend/index");
+const server = require("./server/index");
+
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, ()=>{
-    console.log(`Server is running on localhost:${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
+});
