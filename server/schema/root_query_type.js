3db0d5fa730600003aa2b4aaa66d6cc12d416a4c
Gbaja <gbajaf@yahoo.co.uk>
2018-04-10 14:07:24 +0100

    Wrote root query for graphql using the json server api relates to #8

--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -1,1 +1,24 @@
+const graphql = require("graphql");
+
 const OrganisationType = require("./organisation_type");
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    organisation: {
+      type: OrganisationType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        fetch(` http://localhost:3000/organisations/${args.id}`)
+          .then(res => res.json())
+          .catch(err => {
+            console.log("error: ", err);
+          });
+      }
+    }
+  }
+});
+
+module.exports = RootQuery;
