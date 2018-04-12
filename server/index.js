0ea17b1ab72cbd7ddaccf07e0e176aea8962f12c
Gbaja <gbajaf@yahoo.co.uk>
2018-04-12 17:12:08 +0100

    Enabled static file to load in production

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-const app = require("express")();
+const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const expressGraphQL = require("express-graphql");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const schema = require("./schema/schema");
 
+const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(
@@ -15,6 +18,10 @@ app.use(
     graphiql: true
   })
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+}
 
 app.get("/", (req, res) => {
   res.send("server running");
