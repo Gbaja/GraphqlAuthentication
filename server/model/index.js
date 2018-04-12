3c36b6f4c944c4103d891f6607dc9d40adc00488
Gbaja <gbajaf@yahoo.co.uk>
2018-04-12 13:50:05 +0100

    Heroku database added database to heroku and linked with sequeslize (i think it works, not getting errors so :smile:) relates to #15

--- server/model/index.js
+++ server/model/index.js
@@ -1,12 +1,28 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bridgemap", "postgres", "postgres", {
-  dialect: "postgres",
-  define: {
-    underscored: false
-  }
-});
-
+let sequelize = null;
+if (process.env.USERS_DB_URL) {
+  const match = process.env.DATABASE_URL.match(
+    /postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/
+  );
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect: "postgres",
+    protocol: "postgres",
+    port: match[4],
+    host: match[3],
+    logging: false,
+    dialectOptions: {
+      ssl: true
+    }
+  });
+} else {
+  sequelize = new Sequelize("bridgemap", "postgres", "postgres", {
+    dialect: "postgres",
+    define: {
+      underscored: false
+    }
+  });
+}
 const models = {
   Organisation: sequelize.import("./organisation")
 };
