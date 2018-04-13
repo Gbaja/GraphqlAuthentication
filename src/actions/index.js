76a4508d7a0f9765efd484c2db685fc107af7667
Gbaja <gbajaf@yahoo.co.uk>
2018-04-13 16:10:38 +0100

    Clean up- Take out redux related code fixes #32

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-import { ADD_ORGANISATION } from "./types";
-
-export const addOrganisation = data => {
-  return {
-    type: ADD_ORGANISATION
-  };
-};
--- src/actions/types.js
+++
@@ -1,1 +0,0 @@
-export const ADD_ORGANISATION = "ADD_ORGANISATION";
